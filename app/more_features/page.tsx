1742b8823d7cc68266febb548f8225f992df8e1c
Albert Kim <albertkim@Alberts-MacBook-Pro.local>
2026-04-18 15:03:07 +1000

    bugfix: removed random facts easter egg, as npm package didn't support this node version

--- app/more_features/page.tsx
+++ app/more_features/page.tsx
@@ -1,10 +1,5 @@
-
-import randomFacts from '@dpmcmlxxvi/randomfacts';
-
 export default function MoreFeaturesPage() {
 	
-	const fact = randomFacts.make('Wall-E');
-
 	return (
 		<div className="flex items-center justify-center h-screen flex-col gap-4">
 
@@ -13,15 +8,6 @@ export default function MoreFeaturesPage() {
 				<br />
 				albert.kim1@student.unsw.edu.au
 			</p>
-
-			<p>
-				<br />
-				<br />
-				anywho, this is an unreliable fact about Wall-E:
-				<br />
-				{fact}
-			</p>
-
 		</div>
 	);
 }
