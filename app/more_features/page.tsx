a658d5f3c06dadf637763fe7ba13d53d7a69ce6e
Albert Kim <albertkim@Alberts-MacBook-Pro.local>
2026-04-18 15:49:53 +1000

    optimise: updated easter egg message

--- app/more_features/page.tsx
+++ app/more_features/page.tsx
@@ -3,7 +3,12 @@ export default function MoreFeaturesPage() {
 	return (
 		<div className="flex items-center justify-center h-screen flex-col gap-4">
 			<p>
-				if you have any cool ideas or have found any bugs, please email me:
+				this project intially started with me wanting to automate/assist the process in writing student reports, however,
+				<br />
+				i am now interested in extending the usage of this platform to track and manage students for more than just events/reports.
+				<br />
+				<br />
+				if you have any cool ideas or have found any bugs, PLEASE EMAIL ME:
 				<br />
 				albert.kim1@student.unsw.edu.au
 			</p>
